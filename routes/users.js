146d354c4701130018d8da6d6c649e1d31e5e825
wzjing <wzjing103@gmail.com>
2018-09-05 01:48:38 +0800

    update user profile

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,28 @@
 let express = require('express');
 let router = express.Router();
 let tokenUtil = require('../utils/token');
+let db = require('../database/mongodb');
 
 /* GET users listing. */
-router.get('/*', function(req, res, next) {
+router.get('/*', function (req, res, next) {
     let decoded = tokenUtil.verify(req.cookies.token);
     if (decoded) {
-        res.render('user', {username: decoded.username});
+        db.getUser(decoded.username, (err, user) => {
+            if (err === null) {
+                res.render('user',
+                    {
+                        username: user.username,
+                        nickname: user.nickname,
+                        phone: user.phone,
+                        email: user.email,
+                        birth: function (date) {
+                            return `${date.getFullYear()}-${date.getMonth()}-${date.getDay()}`
+                        }(new Date(user.birth))
+                    });
+            } else {
+                console.log(err)
+            }
+        });
         return
     }
     next()
