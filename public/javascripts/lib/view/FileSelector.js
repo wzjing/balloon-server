e431c3bbb1b54945f0f9f0d6f8da6affaf3c4c49
wzjing <wzjing103@gmail.com>
2019-10-04 18:05:10 +0800

    update file selector

--- public/javascripts/lib/view/FileSelector.js
+++ public/javascripts/lib/view/FileSelector.js
@@ -1,3 +1,15 @@
+export const cssClasses = {
+    LAYOUT: "file-selector--layout",
+    INPUT: "file-selector--input",
+    BUTTON: "file-selector--button"
+};
+
+export const selectors = {
+    LAYOUT_SELECTOR: ".file-selector--layout",
+    INPUT_SELECTOR: ".file-selector--input",
+    BUTTON_SELECTOR: ".file-selector--button"
+};
+
 class FileSelector extends View {
 
     get button() {
@@ -6,6 +18,5 @@ class FileSelector extends View {
 
     constructor(el) {
         super(el);
-
     }
 }
